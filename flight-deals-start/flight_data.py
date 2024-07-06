4bd3a71539a937f1089e214880d15907d1de7e7d
Jonathan Rosas Cedillo <jonathanroscedprogramador@gmail.com>
2024-07-06 09:19:55 -0600

    Terminando proyecto es un codigo que busca el vuelo mas barato de una hoja de calculo

--- flight-deals-start/flight_data.py
+++ flight-deals-start/flight_data.py
@@ -1,1 +1,31 @@
 
+class Flight_Data:
+    def __init__(self):
+        self.cheapest = None
+        self.Depature_airport_code = None
+        self.Arrival_airport_code = None
+        self.outbound_date = None # fecha de salida
+        self.inbound_date = None # fecha entrante
+
+
+    def find_cheapest(self, data: list):
+
+        self.cheapest = data[0][0]
+        for i in range(len(data)):
+            if self.cheapest >= data[i][0]:
+                self.cheapest = data[i][0]
+                self.Depature_airport_code = data[i][1]
+                self.Arrival_airport_code = data[i][2]
+                self.outbound_date = data[i][3]
+                self.inbound_date = data[i][4]
+        print(self.cheapest)
+        print( self.Depature_airport_code)
+        print( self.Arrival_airport_code)
+        print( self.outbound_date)
+        print( self.inbound_date)
+
+
+Object = Flight_Data()
+Object.find_cheapest([(196.69, 'LHR', 'CDG', '2024-07-07', '2025-01-03'), (178.48, 'LGW', 'FRA', '2024-07-07', '2025-01-03')])
+
+
--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,3 +1,55 @@
+import time
+import notification_manager
+from data_manager import Data_Manager
+from flight_search import Flight_Search
+from flight_data import Flight_Data
+
+
+
+
+
+
+sheet = Data_Manager()
+flight = Flight_Search()
+data = Flight_Data()
+
+info = sheet.get_info_sheet()
+for fila in info:
+    try: 
+        sheet.city.append(fila["city"])
+        sheet.iatacode.append(fila["iataCode"])
+        sheet.lowest.append(fila["lowestPrice"])
+        sheet.id.append(fila["id"])
+    except:
+        break
+
+for i in range(len(sheet.city)):
+    # ESTEEE FOR ES PARA PONER MIS IATA CODEEE
+    sheet.iatacode[i] = flight.get_country_code(sheet.city[i])
+    sheet.editing_iata_code(sheet.iatacode[i], sheet.id[i])
+    time.sleep(1)
+
+
+vuelos_baratos = []
+#Este es para ver mivuelo mas baratooo DE CADDAA COUNTRYYY
+for i in range(len(sheet.city)):
+    cheapest = flight.search_flight(iata_code=sheet.iatacode[i], lowest_price=sheet.lowest[i],country=sheet.city[i])
+    if cheapest != None:
+        vuelos_baratos.append(cheapest)
+    time.sleep(1)
+
+print(vuelos_baratos)
+
+## ESTA DE AQUII ES GENEROS PUEDEN SER VUELOOSSS EL VUELO MAS BARATO DE MI SOLICITUDD NO LO COMPARA SIMPLEMENTE ME LO DAAAA
+
+for i in range(len(vuelos_baratos)):
+    mensaje = f"""
+low price alert! Only  £{vuelos_baratos[i][0]} to fly 
+from {vuelos_baratos[i][1]} to {vuelos_baratos[i][2]}, on {vuelos_baratos[i][3]} 
+until {vuelos_baratos[i][4]}.
+    """
+    notification_manager.send_whatsapp(mensaje)
+    time.sleep(2)
 
 
 
@@ -5,49 +57,27 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-API_KEY = os.environ.get("API_KEY")
-API_SECRET = os.environ.get("API_SECRET")
-ACCESS_TOKEN = "c0ifpayZQ1t33pj9y0zpzEw9Ibly"
-token_type = "Bearer"
-
-
-# print(API_SECRET)
+####ESTOO DE AQUIII ME DA EL VUELOOO MAS MAS BARATO DE MI TABLA SI HAY DOS VUELOS QUE CUMPLEN MIS CONDICCIONES ESTE DETERMINA
+### CUAL DE ELLAS ES MAS BARRATOO
+# data.find_cheapest(vuelos_baratos) ### AQUI LE MANDO MI LISTA PARA QUE DE LOS SELECCIONADOS DETERMINE AUN QUIEN ES EL MAS VARATO
 #
-# header = {
-#     "'Authorization": f"Bearer {ACCESS_TOKEN}"
-# }
-# ENDPOINT = "/reference-data/locations/cities"
-# URL = f"https://test.api.amadeus.com/v1{ENDPOINT}"
+# mensaje = f"""
+# low price alert! Only  £{data.cheapest} to fly
+# from {data.Depature_airport_code} to {data.Arrival_airport_code}, on {data.outbound_date}
+# until {data.inbound_date}.
+# """
 #
 #
+# notification_manager.send_whatsapp(mensaje)
+
+
+
+
+
+
+
+
+
+
 
 
